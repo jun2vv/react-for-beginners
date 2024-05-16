c1a05fe0a4c7aaf0525a1b488c9e66789288e12b
jun2vv <cj3696@naver.com>
2024-05-17 08:57:15 +0900

    react effect 예제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev+1);
+  console.log("i run all");
+  useEffect(() => {
+    console.log("tetete")
+  }, []);
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
