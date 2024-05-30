ec4c674dd2d1a2e30dd0c3ef1e4766c46a3169b2
jun2vv <cj3696@naver.com>
2024-05-31 08:57:59 +0900

    useEffect를 활용하여 코인리스트 불러오기

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return function() {
-      console.log("bye :(");
-    }
-  }, []);
-
-  useEffect(function() {
-    console.log("hi :)");
-    return function() {
-      console.log("bye :(");
-    }
-  }, []);
-
-  return <h1>hello</h1>
-}
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, [])
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : (
+        <select>
+          {coins.map((coin) => 
+            <option key={coin.name}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}USD 
+            </option>
+          )}
+        </select>
+      )}
     </div>
   );
 }
+
 
 export default App;
